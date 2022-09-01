a3ad975843854dbd1fcfdb3ea643be3b3cf096ff
Oleksii <shostak.ok@gmail.com>
2022-09-01 21:08:13 +0300

    add new test

--- src/test/java/ProgramTest.java
+++ src/test/java/ProgramTest.java
@@ -18,6 +18,27 @@ public class ProgramTest {
     @Test
     public void tryToGetSquareRoot() {
         int expected = underTest.getSquareRoot(5);
+
         Assert.assertEquals(expected, 2);
+    }
+
+    @Test
+    public void tryToGetSquareRootWithMathFunction() {
+        int expected = underTest.getSquareRoot(9);
+        int actual = (int) Math.sqrt(9);
+
+        Assert.assertEquals(expected, actual);
+    }
+
+    @Test
+    public void tryToGetWrongSquareRoot() {
+        int unexpected = underTest.getSquareRoot(16);
+
+        Assert.assertNotEquals(3, unexpected);
+    }
+
+    @Test
+    public void tryToGetSquareRootWithMultipleValues() {
+
     }
 }
